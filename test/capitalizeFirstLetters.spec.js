c3104a0cf8838b70eb0f47daff419242fd295120
Jonathan FRANCOIS <francois.jonathan13@gmail.com>
2021-06-17 19:16:41 +0200

    + write some test

---
+++ test/capitalizeFirstLetters.spec.js
@@ -0,0 +1,21 @@
+const capitalizeFirstLetters = require("../capitalizeFirstLetters");
+
+test("capitalize take one parameter", () => {
+  expect(capitalizeFirstLetters.length).toEqual(1);
+});
+
+test("capitalize all words in sentence", () => {
+  test("when the sentence has one word", () => {
+    expect(capitalizeFirstLetters("hello")).toEqual("Hello");
+  });
+
+  test("when the sentence has several words", () => {
+    expect(capitalizeFirstLetters("i am learning TDD")).toEqual(
+      "I Am Learning TDD"
+    );
+  });
+
+  test("when the sentence has no word", () => {
+    expect(capitalizeFirstLetters("i am learning tdd")).toBeFalsy();
+  });
+});
